61bf7f6f88493c4f433c7ad659362c52b09f1616
BernardHwang <laohwang0523@gmail.com>
2025-02-13 00:22:17 +0800

    please

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+      "@typescript-eslint/no-explicit-any": "off"
     },
   }),
 ];
