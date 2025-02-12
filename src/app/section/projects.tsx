d7d2eda293a11f064feb7ef7d90b19441b4b6993
BernardHwang <laohwang0523@gmail.com>
2025-02-13 00:08:24 +0800

    ....

--- src/app/section/projects.tsx
+++ src/app/section/projects.tsx
@@ -18,28 +18,26 @@ export const Projects = () => {
           auth: process.env.GITHUB_API_TOKEN
         });
   
-        const response = await octokit.request('GET /users/{username}/repos', {
+        const response = await octokit.request<{ data: Repo[] }>('GET /users/{username}/repos', {
           username: 'BernardHwang',
           headers: {
             'X-GitHub-Api-Version': '2022-11-28'
           }
         });
   
-        setGitRepos(
-          response.data.map((repo: any) => ({
-            name: repo.name,
-            html_url: repo.html_url,
-            description: repo.description ?? "No Description",
-          }))
-        );
-        
+        setGitRepos(response.data.map((repo) => ({
+          name: repo.name,
+          html_url: repo.html_url,
+          description: repo.description ?? "No Description",
+        })));
       } catch (error) {
         console.error("Failed to fetch repos:", error);
       }
     };
-
+  
     getGitRepos();
   }, []);
+  
 
   return (
     <section className="flex flex-col items-center justify-center bg-gray-900 text-white py-10">
